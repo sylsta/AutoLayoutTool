e9fb36a61a352f3ca13434dcd3677aae002e987f
Sylvain THÉRY <sylvain.thery@cnrs.fr>
2021-11-23 18:53:01 +0100

    Landscape/Portrait management (buggy)

--- AutoLayoutTool.py
+++ AutoLayoutTool.py
@@ -27,7 +27,8 @@ from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
 from qgis.PyQt.QtGui import QIcon, QColor, QFont
 from qgis.PyQt.QtWidgets import QAction, QMessageBox
 from qgis.core import QgsProject, QgsPrintLayout, QgsLayoutItemMap, QgsLayoutItemLegend, QgsLayoutPoint, \
-    QgsLayoutItemScaleBar, QgsUnitTypes, QgsLayoutItemPicture, QgsLayoutSize, QgsApplication
+    QgsLayoutItemScaleBar, QgsUnitTypes, QgsLayoutItemPicture, QgsLayoutSize, QgsApplication, QgsLayoutItemPage, \
+    QgsMapSettings
 
 # Initialize Qt resources from file resources.py
 from .resources import *
@@ -211,15 +212,65 @@ class AutoLayoutTool:
 
         layout = QgsPrintLayout(project)
         layout.initializeDefaults()
-        # manager.addLayout(layout)
+        manager.addLayout(layout)
         layout.setName(layoutname)
 
+        # Determine and set best layout orientation
+        xy_ratio = (e.xMaximum() - e.xMinimum()) / (e.yMaximum() - e.yMinimum())
+        layout_width = layout.pageCollection().page(0).pageSize().width()
+        layout_height = layout.pageCollection().page(0).pageSize().height()
+        layout_ratio =  layout_width / layout_height
+        # print("avant : width_map=%r y=%r r=%r" %(layout_width, layout_height, layout_ratio))
+        page_size = QgsApplication.pageSizeRegistry().find(layout.pageCollection().page(0).pageSize())  # eg. 'A4' str
+
+
+        if xy_ratio <= layout_ratio:
+            # portrait
+            layout.pageCollection().page(0).setPageSize(page_size, QgsLayoutItemPage.Orientation.Portrait)
+        else:
+            # landscape
+            layout.pageCollection().page(0).setPageSize(page_size, QgsLayoutItemPage.Orientation.Landscape)
+        layout_width = layout.pageCollection().page(0).pageSize().width()
+        layout_height = layout.pageCollection().page(0).pageSize().height()
+        layout_ratio =  layout_width / layout_height
+        print("après : width_map=%r height_map=%r r=%r" % (layout_width, layout_height, layout_ratio))
         # Add map
         print("Adding map")
         map = QgsLayoutItemMap(layout)
-        map.setRect(0, 0, layout.pageCollection().page(0).pageSize().width(),
-                    layout.pageCollection().page(0).pageSize().height())
+        # print("x_y ratio: {0}".format(xy_ratio))
+        offset_x = 0
+        offset_y = 0
+        if xy_ratio <= layout_ratio:
+            height_map = layout_height
+            width_map = layout_height * xy_ratio
+            offset_x = (layout_width-width_map)/2
+            print(layout_width)
+            print(width_map)
+            print(xy_ratio)
+        else:
+            #landscape
+            height_map = layout_width / xy_ratio
+            width_map = layout_width
+            # print("layout_width")
+            # print(layout_width)
+            # print('height_map')
+            # print(height_map)
+            # print('xy_ratio')
+            # print(xy_ratio)
+            offset_y = (layout_height-height_map) / 2
+        print("offsets")
+        print(offset_x)
+        print(offset_y)
+        #QRect.setRect(width_map, y, w, h)¶
+
+        # map.setRect(offset_x, offset_y, width_map, height_map)
+        ms = QgsMapSettings()
+        ms.setExtent(e)
+
+        map.setRect(0, 0, width_map, height_map)
         map.setExtent(e)
+
+        # map.setExtent(e)
         map.setBackgroundColor(QColor(255, 255, 255, 0))
         map.setFrameEnabled(True)
         layout.addLayoutItem(map)
@@ -269,7 +320,7 @@ class AutoLayoutTool:
         north.attemptMove(QgsLayoutPoint(3, 190, QgsUnitTypes.LayoutMillimeters))
 
         # Finally add layout to the project via its manager
-        manager.addLayout(layout)
+        # manager.addLayout(layout)
 
         self.iface.openLayoutDesigner(layout)
         # new_rect=my_map.get
