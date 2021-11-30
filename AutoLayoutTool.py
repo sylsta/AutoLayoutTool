3e37d685133f2d9779dd0413134a0707c50cdb78
Sylvain THÉRY <sylvain.thery@cnrs.fr>
2021-11-30 11:39:26 +0100

    remove add_action_function cause it do not seems to be able to manage shortcuts for actions Start config action

--- AutoLayoutTool.py
+++ AutoLayoutTool.py
@@ -34,7 +34,8 @@ from qgis.core import QgsProject, QgsPrintLayout, QgsLayoutItemMap, QgsLayoutIte
 import os.path
 # Initialize Qt resources from file resources.py
 from .resources import *
-
+# Import the code for the dialog
+from .AutoLayoutTool_dialog import AutoLayoutToolDialog
 
 class AutoLayoutTool:
     """QGIS Plugin Implementation."""
@@ -79,6 +80,8 @@ class AutoLayoutTool:
         # Check if plugin was started the first time in current QGIS session
         # Must be set in initGui() to survive plugin reloads
         self.first_start = None
+
+        # Use pyCharm debug server
         if self.debug:
             try:
                 import pydevd_pycharm
@@ -86,7 +89,7 @@ class AutoLayoutTool:
             except:
                 pass
 
-    # noinspection PyMethodMayBeStatic
+
 
     def tr(self, message):
         """
@@ -100,93 +103,30 @@ class AutoLayoutTool:
         # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
         return QCoreApplication.translate('AutoLayoutTool', message)
 
-    def add_action(
-            self,
-            icon_path,
-            text,
-            callback,
-            enabled_flag=True,
-            add_to_menu=True,
-            add_to_toolbar=True,
-            status_tip=None,
-            whats_this=None,
-            parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            # Adds plugin icon to Plugins toolbar
-            self.iface.addToolBarIcon(action)
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
-
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
-        icon_path = ':/plugins/create_layout/layout.png'
-        self.my_action = self.add_action(
-            icon_path,
-            text=self.tr(u'Create a new layout based on current extent '),
-            callback=self.run,
-            parent=self.iface.mainWindow())
-
         # will be set False in run()
         self.first_start = True
-        self.my_action = QAction("Test Plugin", self.iface.mainWindow())
-        self.iface.registerMainWindowAction(self.my_action, "Ctrl+I")  # action triggered by Ctrl+I
+
+        # Run entry menu
+        text = self.tr(u'Create a new layout based on current extent ')
+        action = QAction(QIcon(':/plugins/create_layout/layout.png'), text, self.iface.mainWindow())
+        action.triggered.connect(self.run)
+        action.setEnabled(True)
+        action.setStatusTip(text)
+        action.setWhatsThis(text)
+        self.iface.registerMainWindowAction(action, "Ctrl+F4")
+        self.iface.addToolBarIcon(action)
+        self.iface.addPluginToMenu(self.menu, action)
+        self.actions.append(action)
+
+        # Config entry menu
+        action = QAction(self.tr("Config AutoLayoutTool"), self.iface.mainWindow())
+        self.iface.registerMainWindowAction(action, "Ctrl+Shift+F4")
+        self.iface.addPluginToMenu(self.menu, action)
+        action.triggered.connect(self.config)
+
+
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
@@ -195,7 +135,7 @@ class AutoLayoutTool:
                 self.tr(u'&AutoLayoutTool'),
                 action)
             self.iface.removeToolBarIcon(action)
-        self.iface.unregisterMainWindowAction(self.my_action)
+
 
     def create_layout(self, layout_name):
         """
@@ -405,18 +345,29 @@ class AutoLayoutTool:
         north.attemptResize(QgsLayoutSize(8, 13, QgsUnitTypes.LayoutMillimeters))
         north.attemptMove(QgsLayoutPoint(3 + x_offset, map_real_height + y_offset - 15, QgsUnitTypes.LayoutMillimeters))
 
+    def config(self):
+        # Create the dialog with elements (after translation) and keep reference
+        # Only create GUI ONCE in callback, so that it will only load when the plugin is started
+        if self.first_start == True:
+            self.first_start = False
+            self.dlg = AutoLayoutToolDialog()
+
+        # show the dialog
+        self.dlg.show()
+        # Run the dialog event loop
+        result = self.dlg.exec_()
+        # See if OK was pressed
+        if result:
+            pass
+
+        # Do something useful here - delete the line containing pass and
+        # substitute with your code.
+
     def run(self):
         """
         Creates a layout with a map of the current interface extent, with legend, scalebar and north arrow
         :return: None
         """
-        if self.first_start == True:
-            self.first_start = False
-            shortcut = QShortcut(QKeySequence(Qt.ControlModifier + Qt.ShiftModifier + Qt.Key_1),
-                                 self.iface.mainWindow())
-            shortcut.setContext(Qt.ApplicationShortcut)
-            shortcut.activated.connect(self.run)
-
 
         margin = 10
         layout_name = self.tr('Automatic layout')
